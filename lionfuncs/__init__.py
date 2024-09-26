ee53c9f2d9d8cd4c1298f06b063af7e599aee539
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-09-26 16:51:15 -0400

    added tests

---
+++ lionfuncs/__init__.py
@@ -0,0 +1,115 @@
+from .algorithms.jaro_distance import jaro_distance
+from .algorithms.levenshtein_distance import levenshtein_distance
+from .data_handlers.flatten import flatten
+from .data_handlers.nfilter import nfilter
+from .data_handlers.nget import nget
+from .data_handlers.ninsert import ninsert
+from .data_handlers.npop import npop
+from .data_handlers.nset import nset
+from .data_handlers.to_dict import to_dict
+from .data_handlers.to_list import to_list
+from .data_handlers.to_num import to_num
+from .data_handlers.to_str import to_str
+from .data_handlers.unflatten import unflatten
+from .data_handlers.utils import is_homogeneous, is_same_dtype, is_structure_homogeneous
+from .file_handlers.clear_path import clear_path
+from .file_handlers.copy_file import copy_file
+from .file_handlers.create_path import create_path
+from .file_handlers.get_file_size import get_file_size
+from .file_handlers.list_files import list_files
+from .file_handlers.read_file import read_file
+from .file_handlers.save_to_file import save_to_file
+from .file_handlers.split_path import split_path
+from .function_handlers.bcall import bcall
+from .function_handlers.call_decorator import CallDecorator
+from .function_handlers.lcall import alcall, lcall
+from .function_handlers.mcall import mcall
+from .function_handlers.pcall import pcall
+from .function_handlers.rcall import rcall
+from .function_handlers.tcall import tcall
+from .function_handlers.ucall import ucall
+from .function_handlers.utils import force_async
+from .import_handlers.check_import import check_import
+from .import_handlers.get_cpu_architecture import get_cpu_architecture
+from .import_handlers.import_module import import_module
+from .import_handlers.is_import_installed import is_import_installed
+from .import_handlers.list_installed_packages import list_installed_packages
+from .import_handlers.uninstall_package import uninstall_package
+from .import_handlers.update_package import update_package
+from .ln_undefined import LN_UNDEFINED, LionUndefinedType
+from .parsers.as_readable_json import as_readable_json
+from .parsers.choose_most_similar import choose_most_similar
+from .parsers.extract_code_block import extract_code_block
+from .parsers.extract_docstring import extract_docstring
+from .parsers.extract_json_schema import (
+    extract_json_schema,
+    json_schema_to_cfg,
+    json_schema_to_regex,
+)
+from .parsers.function_to_schema import function_to_schema
+from .parsers.md_to_json import extract_json_block, md_to_json
+from .parsers.validate_boolean import validate_boolean
+from .parsers.validate_keys import validate_keys
+from .parsers.validate_mapping import validate_mapping
+from .parsers.xml_parser import dict_to_xml, xml_to_dict
+
+__all__ = [
+    "jaro_distance",
+    "levenshtein_distance",
+    "flatten",
+    "nfilter",
+    "nget",
+    "ninsert",
+    "npop",
+    "nset",
+    "to_dict",
+    "to_list",
+    "to_str",
+    "to_num",
+    "unflatten",
+    "is_homogeneous",
+    "is_same_dtype",
+    "is_structure_homogeneous",
+    "clear_path",
+    "copy_file",
+    "create_path",
+    "get_file_size",
+    "list_files",
+    "read_file",
+    "save_to_file",
+    "split_path",
+    "bcall",
+    "CallDecorator",
+    "alcall",
+    "lcall",
+    "mcall",
+    "pcall",
+    "rcall",
+    "tcall",
+    "ucall",
+    "force_async",
+    "check_import",
+    "get_cpu_architecture",
+    "import_module",
+    "is_import_installed",
+    "list_installed_packages",
+    "uninstall_package",
+    "update_package",
+    "as_readable_json",
+    "choose_most_similar",
+    "extract_code_block",
+    "extract_docstring",
+    "extract_json_schema",
+    "json_schema_to_cfg",
+    "json_schema_to_regex",
+    "function_to_schema",
+    "md_to_json",
+    "extract_json_block",
+    "validate_boolean",
+    "validate_keys",
+    "validate_mapping",
+    "xml_to_dict",
+    "dict_to_xml",
+    "LN_UNDEFINED",
+    "LionUndefinedType",
+]
