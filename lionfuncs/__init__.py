8d0ba4e97f167cbf71f22c94b9940c3e0e5528a7
OceanLi <122793010+ohdearquant@users.noreply.github.com>
2024-10-20 12:19:56 -0400

    modified init

--- lionfuncs/__init__.py
+++ lionfuncs/__init__.py
@@ -1,66 +1,65 @@
-from .algorithms.jaro_distance import jaro_distance
-from .algorithms.levenshtein_distance import levenshtein_distance
-from .data_handlers.flatten import flatten
-from .data_handlers.nfilter import nfilter
-from .data_handlers.nget import nget
-from .data_handlers.ninsert import ninsert
-from .data_handlers.npop import npop
-from .data_handlers.nset import nset
-from .data_handlers.to_dict import to_dict
-from .data_handlers.to_list import to_list
-from .data_handlers.to_num import to_num
-from .data_handlers.to_str import to_str
-from .data_handlers.unflatten import unflatten
-from .data_handlers.utils import (
-    is_homogeneous,
-    is_same_dtype,
-    is_structure_homogeneous,
-)
-from .file_handlers.clear_path import clear_path
-from .file_handlers.copy_file import copy_file
-from .file_handlers.create_path import create_path
-from .file_handlers.get_file_size import get_file_size
-from .file_handlers.is_valid_path import is_valid_path
-from .file_handlers.list_files import list_files
-from .file_handlers.read_file import read_file
-from .file_handlers.save_to_file import save_to_file
-from .file_handlers.split_path import split_path
-from .function_handlers.bcall import bcall
-from .function_handlers.call_decorator import CallDecorator
-from .function_handlers.lcall import alcall, lcall
-from .function_handlers.mcall import mcall
-from .function_handlers.pcall import pcall
-from .function_handlers.rcall import rcall
-from .function_handlers.tcall import tcall
-from .function_handlers.ucall import ucall
-from .function_handlers.utils import force_async
-from .import_handlers.check_import import check_import
-from .import_handlers.get_cpu_architecture import get_cpu_architecture
-from .import_handlers.import_module import import_module
-from .import_handlers.is_import_installed import is_import_installed
-from .import_handlers.list_installed_packages import list_installed_packages
-from .import_handlers.uninstall_package import uninstall_package
-from .import_handlers.update_package import update_package
-from .integrations.pandas_ import to_csv, to_df, to_excel
-from .integrations.pydantic_ import break_down_annotation, new_model
+import logging
+
+from .algo.cosine_similarity import cosine_similarity
+from .algo.hamming_similarity import hamming_similarity
+from .algo.jaro_distance import jaro_winkler_similarity
+from .algo.levenshtein_distance import levenshtein_similarity
+from .data.flatten import flatten
+from .data.nfilter import nfilter
+from .data.nget import nget
+from .data.ninsert import ninsert
+from .data.nmerge import nmerge
+from .data.npop import npop
+from .data.nset import nset
+from .data.to_dict import to_dict
+from .data.to_list import to_list
+from .data.to_num import to_num
+from .data.to_str import to_str
+from .data.unflatten import unflatten
+from .data.utils import is_homogeneous, is_same_dtype, is_structure_homogeneous
+from .file.chunk_by_chars import chunk_by_chars
+from .file.chunk_by_tokens import chunk_by_tokens
+from .file.chunk_content import chunk_content
+from .file.clear_path import clear_path
+from .file.copy_file import copy_file
+from .file.create_path import create_path
+from .file.dir_to_files import dir_to_files
+from .file.file_to_chunks import file_to_chunks
+from .file.get_file_size import get_file_size
+from .file.is_valid_path import is_valid_path
+from .file.list_files import list_files
+from .file.read_file import read_file
+from .file.save_to_file import save_to_file
+from .file.split_path import split_path
+from .func.bcall import bcall
+from .func.call_decorator import CallDecorator
+from .func.lcall import alcall, lcall
+from .func.mcall import mcall
+from .func.pcall import pcall
+from .func.rcall import rcall
+from .func.tcall import tcall
+from .func.ucall import ucall
+from .func.utils import force_async
+from .integrations.pandas_ import read_csv, read_json, to_csv, to_df, to_excel
+from .integrations.pydantic_ import break_down_pydantic_annotation, new_model
 from .ln_undefined import LN_UNDEFINED, LionUndefinedType
 from .note import Note, note
-from .parsers.as_readable_json import as_readable_json
-from .parsers.choose_most_similar import choose_most_similar
-from .parsers.extract_code_block import extract_code_block
-from .parsers.extract_docstring import extract_docstring
-from .parsers.extract_json_schema import (
+from .parse.as_readable_json import as_readable_json
+from .parse.choose_most_similar import choose_most_similar
+from .parse.extract_code_block import extract_code_block
+from .parse.extract_json_block import extract_block
+from .parse.extract_json_schema import (
     extract_json_schema,
     json_schema_to_cfg,
     json_schema_to_regex,
+    print_cfg,
 )
-from .parsers.function_to_schema import function_to_schema
-from .parsers.md_to_json import extract_json_block, md_to_json
-from .parsers.validate_boolean import validate_boolean
-from .parsers.validate_keys import validate_keys
-from .parsers.validate_mapping import validate_mapping
-from .parsers.validate_str import validate_str
-from .parsers.xml_parser import dict_to_xml, xml_to_dict
+from .parse.function_to_schema import function_to_schema
+from .parse.fuzzy_parse_json import fuzzy_parse_json
+from .parse.md_to_json import md_to_json
+from .parse.validate_boolean import validate_boolean
+from .parse.validate_mapping import validate_mapping
+from .parse.xml_parser import dict_to_xml, xml_to_dict
 from .utils import (
     copy,
     format_deprecation_msg,
@@ -69,35 +68,45 @@ from .utils import (
     get_class_objects,
     get_file_classes,
     insert_random_hyphens,
+    is_same_dtype,
     run_pip_command,
     time,
     unique_hash,
 )
+from .version import __version__
+
+logging.basicConfig(level=logging.INFO)
 
 __all__ = [
-    "get_bins",
-    "jaro_distance",
-    "levenshtein_distance",
+    "cosine_similarity",
+    "hamming_similarity",
+    "jaro_winkler_similarity",
+    "levenshtein_similarity",
     "flatten",
     "nfilter",
     "nget",
     "ninsert",
+    "nmerge",
     "npop",
     "nset",
     "to_dict",
     "to_list",
     "to_num",
     "to_str",
-    "to_df",
     "unflatten",
-    "format_deprecation_msg",
     "is_homogeneous",
     "is_same_dtype",
     "is_structure_homogeneous",
+    "chunk_by_chars",
+    "chunk_by_tokens",
+    "chunk_content",
     "clear_path",
     "copy_file",
     "create_path",
+    "dir_to_files",
+    "file_to_chunks",
     "get_file_size",
+    "is_valid_path",
     "list_files",
     "read_file",
     "save_to_file",
@@ -112,31 +121,31 @@ __all__ = [
     "tcall",
     "ucall",
     "force_async",
-    "check_import",
-    "get_cpu_architecture",
-    "import_module",
-    "is_import_installed",
-    "list_installed_packages",
-    "uninstall_package",
-    "update_package",
-    "LN_UNDEFINED",
-    "LionUndefinedType",
+    "to_df",
+    "to_csv",
+    "read_csv",
+    "read_json",
+    "to_excel",
+    "break_down_pydantic_annotation",
+    "new_model",
     "as_readable_json",
     "choose_most_similar",
     "extract_code_block",
-    "extract_docstring",
+    "extract_block",
     "extract_json_schema",
-    "json_schema_to_cfg",
     "json_schema_to_regex",
+    "json_schema_to_cfg",
+    "print_cfg",
     "function_to_schema",
-    "extract_json_block",
     "md_to_json",
-    "is_valid_path",
     "validate_boolean",
-    "validate_keys",
     "validate_mapping",
-    "dict_to_xml",
     "xml_to_dict",
+    "dict_to_xml",
+    "fuzzy_parse_json",
+    "note",
+    "Note",
+    "format_deprecation_msg",
     "unique_hash",
     "insert_random_hyphens",
     "get_file_classes",
@@ -145,11 +154,9 @@ __all__ = [
     "time",
     "copy",
     "run_pip_command",
-    "validate_str",
-    "break_down_annotation",
-    "to_csv",
-    "to_excel",
-    "new_model",
-    "Note",
-    "note",
+    "format_deprecation_msg",
+    "get_bins",
+    "LN_UNDEFINED",
+    "LionUndefinedType",
+    "__version__",
 ]
